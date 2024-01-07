6a9ad2d58c50c8cbbffe660dfc98b5664c9f05b8
Nevets <12600557+Nevets10@users.noreply.github.com>
2024-01-07 14:21:30 -0800

    Add about section

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,8 +1,20 @@
 const mainDiv = document.createElement("div");
 
-const h1 = document.createElement("h1");
-h1.textContent = "Heck's Kitchen";
+const titleEl = document.createElement("h1");
+titleEl.textContent = "Heck's Kitchen";
+mainDiv.appendChild(titleEl);
 
-mainDiv.appendChild(h1);
+const aboutEl = document.createElement("div");
+const titleEl1 = document.createElement("h2");
+titleEl1.textContent = "About Us";
+
+const textEl1 = document.createElement("p");
+textEl1.textContent =
+	"Bacon ipsum dolor amet chuck pastrami rump, ham hock meatloaf tongue ball tip pig. Frankfurter salami kielbasa prosciutto drumstick jerky jowl alcatra. Beef ribs alcatra tail fatback cupim, rump burgdoggen strip steak ham. Pork chop flank brisket shoulder bacon cow chislic swine buffalo jowl tri-tip short ribs hamburger doner spare ribs. Chislic salami flank fatback. Tri-tip capicola tenderloin pork chop shank beef ribs pancetta pastrami jerky.";
+
+aboutEl.appendChild(titleEl1);
+aboutEl.appendChild(textEl1);
+
+mainDiv.appendChild(aboutEl);
 
 export default mainDiv;
