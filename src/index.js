83e52d1096125b3e70305dec31b0f3c9d0d0e198
Nevets <12600557+Nevets10@users.noreply.github.com>
2024-01-07 14:05:04 -0800

    Add home.js, import home into index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello there.");
+import home from "./modules/home";
+console.log(home);
+const contentEl = document.getElementById("content");
+contentEl.appendChild(home);
---
+++ src/modules/home.js
@@ -0,0 +1,8 @@
+const mainDiv = document.createElement("div");
+
+const h1 = document.createElement("h1");
+h1.textContent = "Heck's Kitchen";
+
+mainDiv.appendChild(h1);
+
+export default mainDiv;
